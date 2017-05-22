246942ec01c605f9427653136c322f824d02eaa9
Paul Malikov <vokilam@gmail.com>
2017-05-23 00:39:03 +0300

    Fix console tests

--- app/src/test/java/com/vokilam/test_spec_mockito/ExampleUnitTestKotlinSpek.kt
+++ app/src/test/java/com/vokilam/test_spec_mockito/ExampleUnitTestKotlinSpek.kt
@@ -1,20 +1,29 @@
 package com.vokilam.test_spec_mockito
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.junit.Test
 import org.mockito.Mockito
 
 import org.junit.Assert.assertEquals
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
 
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
+@RunWith(JUnitPlatform::class)
 class ExampleUnitTestKotlinSpek : Spek({
-    it("test") {
-        val activity = Mockito.mock(MainActivity::class.java)
-        assertEquals(4, (2 + 2).toLong())
+    given("MainActivity") {
+        on("mock activity") {
+            val activity = Mockito.mock(MainActivity::class.java)
+            it("test") {
+                Mockito.verify(activity)
+            }
+        }
     }
 })
